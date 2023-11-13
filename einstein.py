2f542bb4af3f3e3d227b56fb35b8e854680a2ba4
scheinbein <150675203+scheinbein@users.noreply.github.com>
2023-11-13 09:41:43 -0300

    einstein.py

---
+++ einstein.py
@@ -0,0 +1,7 @@
+massa= int(input("Digite a massa:"))
+caoquadrado= 9000000000000
+print("A energia do corpo é" ,int(massa*caoquadrado), "Joules.") 
+
+
+#A velocidade da luz na lista está errada, ela é 300000000.
+
